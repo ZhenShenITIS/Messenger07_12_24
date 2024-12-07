4770ad0c690af8d24505c06d44612bc5c04f8cde
ZhenShen.ITIS <ewgenii20066@gmail.com>
2024-12-07 09:05:20 +0400

    Евгений Переверзев - сделал сообщение

---
+++ src/main/java/ru/messenger/Message.java
@@ -0,0 +1,54 @@
+package ru.messenger;
+/*
+    - Отправитель (пользователь)
+    - Получатель  (пользователь)
+    - Текст
+    - Дата-время
+ */
+public class Message {
+    private User sender;
+    private User recipient;
+    private String text;
+    private String date;
+
+    public Message() {}
+
+    public Message(User sender, User recipient, String text, String date) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.text = text;
+        this.date = date;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
